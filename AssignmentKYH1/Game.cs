73a91f7c16278032b5ff73e0168725b32ff24a08
AlexisBriceno98 <alex00534@hotmail.com>
2022-09-13 15:37:51 +0200

    Första del klar!

--- AssignmentKYH1/Game.cs
+++ AssignmentKYH1/Game.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,37 @@ namespace AssignmentKYH1
         public void Run()
         {
 
-        }
-    }
+            Random rand = new Random();
+            int nummer = rand.Next(0, 100);
+            
+            while (true)
+
+            {
+                Console.WriteLine("Gissa ett tal mellan 1 och 100");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess > nummer)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Du gissade fel, talet är lägre");
+                }
+                else if (guess < nummer)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Du gissade fel, talet är högre");
+                }
+                else if (guess == nummer)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Du gissade rätt!");
+                    break;
+                    {
+
+
+
+                    }
+                }
+ 
+            }
+    }   }
 }
