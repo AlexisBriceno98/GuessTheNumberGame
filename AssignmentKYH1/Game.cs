850acbc803f93ba5731f51d5fb0dd7483ce00729
AlexisBriceno98 <alex00534@hotmail.com>
2022-09-16 01:38:49 +0200

    Update

--- AssignmentKYH1/Game.cs
+++ AssignmentKYH1/Game.cs
@@ -15,27 +15,33 @@ namespace AssignmentKYH1
 
             Random rand = new Random();
             int nummer = rand.Next(0, 100);
-            
+            int gissningar = 0;
             while (true)
-
+                
             {
                 Console.WriteLine("Gissa ett tal mellan 1 och 100");
                 int guess = Convert.ToInt32(Console.ReadLine());
+                {
 
+                }
                 if (guess > nummer)
                 {
                     Console.Clear();
                     Console.WriteLine("Du gissade fel, talet är lägre");
+                    gissningar++;
                 }
                 else if (guess < nummer)
                 {
                     Console.Clear();
                     Console.WriteLine("Du gissade fel, talet är högre");
+                    gissningar++;
                 }
                 else if (guess == nummer)
                 {
                     Console.Clear();
                     Console.WriteLine("Du gissade rätt!");
+                    gissningar++;
+                    Console.WriteLine($"Det tog dig {gissningar} gissningar");
                     break;
                     {
 
